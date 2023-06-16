c29198029ea81071cbe6b5c2ab10745ba89cced6
BJuraeva <barnojonjuraeva@gmail.com>
2023-06-15 22:28:43 -0400

    review13

---
+++ src/main/java/com/syntax/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.syntax.utils;
+
+public class Constants {
+    public static final String EXCEL_FILE_PATH = "C:\\Users\\16465\\IdeaProjects\\JavaReview\\Files\\HRMSEmployeesData.xlsx";
+
+    public static final String CONFIG_FILE_PATH="Files/config.properties";
+}
